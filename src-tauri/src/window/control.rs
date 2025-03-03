f74d2493a4242df07e5071512a436dc52702d454
chenzibo <qw.54@163.com>
2025-03-03 10:19:55 +0800

    fix:修复显示控制窗口的入参泛型

--- src-tauri/src/window/control.rs
+++ src-tauri/src/window/control.rs
@@ -2,7 +2,7 @@ use super::quick_window::show_previous_window;
 use crate::CONTROL_WINDOW_LABEL;
 use tauri::{self, Error, Manager, Runtime};
 
-pub fn show_control_window<R: Runtime>(app: &tauri::AppHandle) -> Result<(), Error> {
+pub fn show_control_window<R: Runtime>(app: &tauri::AppHandle<R>) -> Result<(), Error> {
     let Some(control_window) = app.get_webview_window(CONTROL_WINDOW_LABEL) else {
         return Ok(());
     };
